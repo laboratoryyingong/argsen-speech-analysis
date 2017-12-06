6013a4bf1de0edf958693ac0ef1dd8ad25e94db2
YIN GONG <max.g.laboratory@gmail.com>
2017-12-06 17:26:32 +1030

    Work #06Dec2017
    1. add judge script
    2. re-organise script structure

--- app/public/js/index.js
+++ app/public/js/index.js
@@ -1,8 +1,28 @@
+/**
+ * @file: ./app/public/js/index.js
+ * @author: yin_gong<max.g.laboratory@gmail.com>
+ */
+var socket = io.connect('http://localhost:3999');
+
+if (!window.jQuery) {
+    var script = document.createElement('script');
+    script.type = "text/javascript";
+    script.src = "https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js";
+    document.getElementsByTagName('head')[0].appendChild(script);
+}
+
 var mediaConstraints = {
     audio: true
 };
 
 navigator.getUserMedia(mediaConstraints, onMediaSuccess, onMediaError);
+
+//socket
+
+socket.on('connect', function (data) {
+    socket.emit('join', 'Hello World from client');
+});
+
 
 function onMediaSuccess(stream) {
     var mediaRecorder = new MediaStreamRecorder(stream);
